5e6ebca172d6a53241801cf6c9e330fb7501485f
UNNATI SHRI <unnatiprofessional@gmail.com>
2026-03-01 23:35:41 +0530

    Added initial LeetCode solution file

---
+++ Leetcode Problems JAVA/sqrt.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int mySqrt(int x) {
+
+        if (x < 2) return x;
+
+        int left = 1;
+        int right = x / 2;
+        int answer = 0;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (mid <= x / mid) {
+                answer = mid;      // possible answer
+                left = mid + 1;    // try bigger
+            } else {
+                right = mid - 1;   // go smaller
+            }
+        }
+
+        return answer;
+    }
+}
